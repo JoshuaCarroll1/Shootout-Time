7d06a7e8710e2529d7b371e2866a0487fa30d88d
JoshuaCarroll1 <joshcarroll8457@gmail.com>
2023-03-04 20:48:04 +0000

    Add function for buttons and direction of Ai

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 /* jshint esversion: 11 */
 
 let timer;
-let timerSeconds = 10;
+let timerSeconds = 100;
 const countdown = document.getElementById("countdown");
 const playBtn = document.getElementById("play");
 const goalSpan = document.getElementById("goal");
 const missedSpan = document.getElementById("missed");
 let goals = 0;
 let missed = 0;
+const controlButtons = document.querySelectorAll(".control-buttons");
+let gameImg = document.getElementById("game-pic");
 
 playBtn.addEventListener("click", timerSlot);
 
@@ -19,6 +21,29 @@ function incrementGoal()    {
 function incrementMiss()    {
     missed++;
     missedSpan.innerText = missed;
+}
+
+function randomAiDirection()  {
+    const aiChoice = math.floor(math.random() * 5 + 1);
+
+    switch (aiChoice) {
+        case 1:
+            randomAiDirection = 'bottom-left';
+            break;
+        case 2:
+            randomAiDirection = 'bottom-right';
+            break;
+        case 3:
+            randomAiDirection = 'middle';
+            break;
+        case 4:
+            randomAiDirection = 'top-left';
+            break;
+        case 5:
+            randomAiDirection = 'top-right';
+            break;
+
+    }
 }
 
 function displayImage() {
